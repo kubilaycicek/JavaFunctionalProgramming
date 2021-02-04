138b951db384dc8392cacdf8cd0f9afab6ba75f6
kubilaycicek <kubilaycicek0693@gmail.com>
2021-02-04 22:02:30 +0300

    Function

---
+++ javafunctional/src/main/java/functionalinterface/_Function.java
@@ -0,0 +1,24 @@
+package functionalinterface;
+
+import java.util.function.Function;
+
+public class _Function {
+    public static void main(String[] args) {
+        int increment = incrementByOne(1);
+        System.out.println(increment);
+
+
+        Integer increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+
+        incrementByOneFunction.
+
+    }
+
+    static Function<Integer, Integer> incrementByOneFunction = number -> number+1;
+
+    static int incrementByOne(int number) {
+        return number + 1;
+    }
+
+}
