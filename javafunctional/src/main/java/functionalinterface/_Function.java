853da8b38d4e59cd5b71941f489a9e60cb75c598
kubilaycicek <kubilaycicek0693@gmail.com>
2021-02-04 22:09:53 +0300

    Function AndThen

--- javafunctional/src/main/java/functionalinterface/_Function.java
+++ javafunctional/src/main/java/functionalinterface/_Function.java
@@ -11,9 +11,18 @@ public class _Function {
         Integer increment2 = incrementByOneFunction.apply(1);
         System.out.println(increment2);
 
+        Integer multiply = multipleBy10Function.apply(3);
+        System.out.println(multiply);
+
+        System.out.println(addBy1AndThenMultiplyBy10.apply(4));
+
     }
 
-    static Function<Integer, Integer> incrementByOneFunction = number -> number+1;
+    static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
+    static Function<Integer, Integer> multipleBy10Function = number -> number * 10;
+
+    static Function<Integer, Integer> addBy1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multipleBy10Function);
 
     static int incrementByOne(int number) {
         return number + 1;
