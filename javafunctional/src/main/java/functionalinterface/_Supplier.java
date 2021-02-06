7c1841260ab60832a50468232ef4106bfa55a2b4
kubilaycicek <kubilaycicek0693@gmail.com>
2021-02-06 12:18:23 +0300

    Supplier

---
+++ javafunctional/src/main/java/functionalinterface/_Supplier.java
@@ -0,0 +1,25 @@
+package functionalinterface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class _Supplier {
+    public static void main(String[] args) {
+        System.out.println(getDBConnectionUrl());
+        System.out.println(getDBConnectionUrlSupplier.get());
+    }
+
+    static String getDBConnectionUrl() {
+        return "jdbc://localhost:5432/users";
+    }
+
+    static Supplier<String> getDBConnectionUrlSupplier = ()
+            -> "jdbc://localhost:5432/users";
+
+    static Supplier<List<String>> getDBConnectionUrlsSupplier = ()
+            -> Arrays.asList(
+            "jdbc://localhost:5432/users",
+            "jdbc://localhost:5432/products",
+            "jdbc://localhost:5432/categories");
+}
